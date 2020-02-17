bb94cf49c54699aeaae26adc52e408e629cc4e60
Ryan <ryan@Ryans-MacBook-Pro-4.local>
2020-02-17 17:04:08 -0600

    FINAL

--- my-app/src/components/AddTodo.js
+++ my-app/src/components/AddTodo.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
 
 export class AddTodo extends Component {
   state = {
     title: ''
   }
-
+onSubmit = (e) => {
+  e.preventDefault();
+  this.props.addTodo(this.state.title);
+  this.setState({title: ''});
+    
+}
   onChange = (e) => this.setState({ [e.target.name]:
       e.target.value
   });
 
   render() {
     return (
-      <form style={{ display: 'flex' }}>
+      <form onSubmit={this.onSubmit} style={{ display: 'flex' }}>
         <input
           type="text"
           name="title"
@@ -30,6 +36,10 @@ export class AddTodo extends Component {
       </form>
     )
   }
+}
+
+AddTodo.propTypes = {
+  addTodo: PropTypes.func.isRequired
 }
 
 export default AddTodo
